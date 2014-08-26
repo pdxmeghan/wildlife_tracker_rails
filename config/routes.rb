80189672dfa75afc4af63fdfb1425cf666fdb9aa
Meghan Lindsley & Scott Dudley <student@epicodus.com>
2014-08-26 12:28:27 -0700

    Add ability to create and add a new species to the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'species#index'})
-  match('/species', {:via => :get, :to => 'species#index'})
+  match('species', {:via => :get, :to => 'species#index'})
+  match('species/new', {:via => :get, :to => 'species#new'})
+  match('species', {:via => :post, :to => 'species#create'})
 end
